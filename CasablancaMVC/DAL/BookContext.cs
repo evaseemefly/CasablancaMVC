99eeb40dcde21b6743ba1b5e820c8b55df059249
t420s <evaseemefly@126.com>
2017-06-04 15:56:32 +0800

    bootstrap+knockout.js实现的示例

---
+++ CasablancaMVC/DAL/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CasablancaMVC.Models;
+
+namespace CasablancaMVC.DAL
+{
+    public class BookContext:DbContext
+    {
+        public BookContext() : base("BookContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CasablancaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CasablancaMVC.MvcApplication" Language="C#" %>

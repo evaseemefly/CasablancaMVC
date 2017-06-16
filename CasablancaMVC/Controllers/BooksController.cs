f4b138fe8573c6e0c11d003b61d05f34cc4498af
liusihan-单位 <evaseemefly@126.com>
2017-06-16 16:52:45 +0800

    实现books的一些操作

--- CasablancaMVC/Controllers/BooksController.cs
+++ CasablancaMVC/Controllers/BooksController.cs
@@ -61,7 +61,7 @@ namespace CasablancaMVC.Controllers
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        //[ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,AuthorId,Title,Isbn,Synopsis,Description,ImageUrl")] Book book)
         {
             if (ModelState.IsValid)

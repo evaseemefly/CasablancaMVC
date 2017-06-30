f9f41504bee325b6742d18d2d5962bb984bb3a6f
evaseemefly <evaseemefly@126.com>
2017-06-30 16:13:37 +0800

    6月30日 添加部分内容

--- ShoppingCart/Scripts/ViewModels/CartItemViewModel.js
+++ ShoppingCart/Scripts/ViewModels/CartItemViewModel.js
@@ -6,29 +6,33 @@
 
     self.showButton = params.showButton;
 
+    //提交表单时调用的函数
     self.upsertCartItem = function (form) {
+        //验证表单是否有效
         if (!$(form).valid())
             return false;
-
+        //设置可监控变量sending为true
         self.sending(true);
-
+        //准备传回给服务器的对象
         var data = {
             id: self.cartItem.id,
             cartId: self.cartItem.book.id,
+            bookId:self.cartItem.book.id,
             quantity: self.cartItem.quantity()
         };
-
+        //ajax执行请求
         $.ajax({
             url: '/api/caritems',
             type: self.cartItem.id == undefined ? 'post' : 'put',
             contentType: 'application/json',
             data: ko.toJSON(data)
         })
-        .success(self.successfulSave)
+        .success(self.successfulSave)   //设置成功、失败及完成时 的回调函数
         .error(self.errorSave)
         .complete(function(){self.sending(false)});
 
         self.successfulSave = function (data) {
+
             var msg = '<div class="alert aler-success"><strong>成功！</strong>已经添加';
             if (self.cartItem.id == undefined)
                 msg += '添加至';
